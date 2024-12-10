8c9a726150d19ecb51929e0141da1489de1e9daa
Zoro <jooptimmer@live.nl>
2024-12-10 20:26:42 +0100

    v0.0.1

--- query.go
+++ query.go
@@ -1,6 +1,7 @@
 package orderedquery
 
 import (
+	"net/url"
 	"strings"
 )
 
@@ -15,7 +16,18 @@ func NewQuery(capacity int) *Query {
 }
 
 func (q *Query) Add(key, value string) {
-	q.pairs = append(q.pairs, [2]string{key, value})
+	decodedKey, _ := url.QueryUnescape(key)
+	decodedValue, _ := url.QueryUnescape(value)
+	q.pairs = append(q.pairs, [2]string{decodedKey, decodedValue})
+}
+
+func (q *Query) Get(key string) string {
+	for _, pair := range q.pairs {
+		if pair[0] == key {
+			return pair[1]
+		}
+	}
+	return ""
 }
 
 func (q *Query) Encode() string {
@@ -24,9 +36,37 @@ func (q *Query) Encode() string {
 		if i > 0 {
 			b.WriteByte('&')
 		}
-		b.WriteString(p[0])
+		b.WriteString(url.QueryEscape(p[0]))
 		b.WriteByte('=')
-		b.WriteString(p[1])
+		b.WriteString(url.QueryEscape(p[1]))
 	}
 	return b.String()
+}
+
+func ParseQuery(queryString string) (*Query, error) {
+	if len(queryString) > 0 && queryString[0] == '?' {
+		queryString = queryString[1:]
+	}
+
+	if queryString == "" {
+		return NewQuery(0), nil
+	}
+
+	pairs := strings.Split(queryString, "&")
+	q := NewQuery(len(pairs))
+
+	for _, pair := range pairs {
+		if pair == "" {
+			continue
+		}
+
+		kv := strings.SplitN(pair, "=", 2)
+		if len(kv) == 1 {
+			q.Add(kv[0], "")
+		} else {
+			q.Add(kv[0], kv[1])
+		}
+	}
+
+	return q, nil
 }
